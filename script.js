656ca6132ca848f97403e9a1a993b26ea99493ea
Toadieyt <rudyblondyn6@gmail.com>
2023-10-05 22:31:17 +0200

    05102023

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+const eng = document.getElementById('eng');
+const pl = document.getElementById('pl');
+const title = document.querySelector('.title');
+const intr = document.querySelector('.introduce_web');
+
+pl.classList.add('non-active');
+
+pl.addEventListener('click', () => {
+    title.innerHTML = 'Witaj na naszej stronie!';
+    intr.innerHTML = 'Strona internetowa stworzona z myślą o 8-klasistach. Umożliwia naukę materiału niezbędnego do napisania rgzaminu 8-klasisty na wysokim poziomie poprzez grę typu "Milionerzy" oraz wiele materiałów do nauki.';
+    pl.classList.add('non-active');
+    eng.classList.remove('non-active');
+});
+
+eng.addEventListener('click', () => {
+    title.innerHTML = 'Welcome to our website!';
+    intr.innerHTML = 'A website created for 8th graders from Poland. It allows you to learn the material necessary to write the 8th grade exam at a high level through a game like "Millionaires" and many learning materials.';
+    eng.classList.add('non-active');
+    pl.classList.remove('non-active');
+});
+
 const btn = document.querySelector('.btn');
 const portfolio = document.querySelector('.portfolio');
-const title = document.querySelector('.title');
-
 
 btn.addEventListener('click', () => {
     portfolio.classList.remove('non-active');
